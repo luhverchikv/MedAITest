1d7c652671992783502ae666090ed29b4be93ede
luhverchikv <luhverchikv@icloud.com>
2026-03-30 14:29:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,11 @@ INPUT_FILE = "tests_data.txt"
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 DEFAULT_MODEL = "gpt-3.5-turbo"
 
+
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY", "")
+DEEPSEEK_MODEL = "deepseek-chat"
+DEEPSEEK_BASE_URL = "https://api.deepseek.com"
+MAX_QUESTIONS = None  # или ограничение (например 50)
+SAVE_PROGRESS_EVERY = 10
+
+
